a1e9b75b50c5f048de2cd8664d32eca9db90deaf
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-10-05 21:07:19 +0530

    Add variables and logic for #bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,33 @@
 def bubble_sort(array)
 
+  length = array.length
+
+  swapped = false
+
+  (length - 1).times do 
+
+      (length - 1).times do |i|
+      
+        if array[i] > array[i + 1]
+
+          x = array[i]
+          y = array[i + 1]
+          array[i] = y
+          array[i + 1] = x
+          swapped = true
+
+        end
+
+      end
+
+      if swapped == false
+        break
+      end
+
+  end
+    
+  p array
+
 end
+
+bubble_sort([4,3,78,2,0,2])
